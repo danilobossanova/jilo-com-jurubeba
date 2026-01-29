a5b9ffe43f0cdddba27290d4853f0cabaae4e6ed
Danilo Bossanova <danilo.bossanova@hotmail.com>
2026-01-29 08:16:47 -0300

    Create JiloComJurubebaApplication.java

---
+++ src/main/java/com/grupo3/postech/jilocomjurubeba/JiloComJurubebaApplication.java
@@ -0,0 +1,13 @@
+package com.grupo3.postech.jilocomjurubeba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiloComJurubebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiloComJurubebaApplication.class, args);
+	}
+
+}
